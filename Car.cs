4c6a587f61621d7124b4c01b1c38e221e413a21b
Elev <Elev@CNCN-WS054>
2023-11-15 09:42:59 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_2023
+{
+    public class Car
+    {
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+        public Car(string colorName, string nameName, int maxSpeedValue, int yearValue)
+        {
+            name= nameName;
+            maxSpeed = maxSpeedValue;
+            year = yearValue;
+            color = colorName;
+        }
+
+        public void fullThrottle() {
+        Console.WriteLine(name + "is going at the speed of" + maxSpeed);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_2023
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang","white",20201,100);
+            Car Opel = new Car("Astra","blue",2020,120);
+            Ford.fullThrottle();
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Ford.fullThrottle();
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Opel.fullThrottle();
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
